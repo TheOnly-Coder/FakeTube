bf480bde42e0776145977878e8719e7cd44fbf98
Z User <z@container>
2026-08-12 04:30:25 +0000

    Fix logo triangle: polygon now fills the full viewBox

--- js/components.js
+++ js/components.js
@@ -1,7 +1,7 @@
 import { getCurrentUser, onAuthChange, logout, login, signup } from './auth.js';
 import { showToast, getInitials, escapeHtml } from './utils.js';
 
-const LOGO_SVG = `<svg viewBox="0 0 24 24"><polygon points="9.5,7.5 9.5,16.5 16.5,12"/></svg>`;
+const LOGO_SVG = `<svg viewBox="0 0 24 24"><polygon points="6,3 6,21 21,12"/></svg>`;
 const SEARCH_SVG = `<svg viewBox="0 0 24 24"><path d="M20.87,20.17l-5.59-5.59C16.35,13.35,17,11.75,17,10c0-3.87-3.13-7-7-7s-7,3.13-7,7s3.13,7,7,7c1.75,0,3.35-.65,4.58-1.72l5.59,5.59L20.87,20.17z M10,16c-3.31,0-6-2.69-6-6s2.69-6,6-6s6,2.69,6,6S13.31,16,10,16z"/></svg>`;
 const UPLOAD_SVG = `<svg viewBox="0 0 24 24"><path d="M17,10.5V7c0-.55-.45-1-1-1H4c-.55,0-1,.45-1,1v10c0,.55.45,1,1,1h12c.55,0,1-.45,1-1v-3.5l4,4v-11L17,10.5z"/></svg>`;
 const CLOSE_SVG = `<svg viewBox="0 0 24 24"><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41z"/></svg>`;
