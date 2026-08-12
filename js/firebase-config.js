fc8dcf55f4701aa055956f8e5d26fdea78eb7101
Z User <z@container>
2026-08-12 02:07:10 +0000

    Update Firebase config to faketube-d10ee project

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,14 +5,15 @@ import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGNgO66TR_YHLmiOCAYItn0a2gEAT23a0",
-  authDomain: "scream-of-justice.firebaseapp.com",
-  databaseURL: "https://scream-of-justice-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "scream-of-justice",
-  storageBucket: "scream-of-justice.firebasestorage.app",
-  messagingSenderId: "178618721941",
-  appId: "1:178618721941:web:a8ff42aaff277e1b405d9c",
-  measurementId: "G-H8KWQD8FT4"
+  apiKey: "AIzaSyAj9nZ6XGnK_VdGeqHg47QpN8dt1T2C3zQ",
+  authDomain: "faketube-d10ee.firebaseapp.com",
+  // IMPORTANT: Create the Realtime Database in Firebase Console first,
+  // then paste its URL below (e.g. https://faketube-d10ee-default-rtdbREGION.firebaseio.com)
+  databaseURL: "https://faketube-d10ee-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "faketube-d10ee",
+  storageBucket: "faketube-d10ee.firebasestorage.app",
+  messagingSenderId: "926439479138",
+  appId: "1:926439479138:web:7842d77cceb0d044343e3b"
 };
 
 const app = initializeApp(firebaseConfig);
